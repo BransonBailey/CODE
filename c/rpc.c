2fb52622959daa48a318a124dc10fcd070e46c38
Branson Bailey <bransonbailey1@gmail.com>
2026-03-29 00:02:31 -0400

    started learning C!

---
+++ c/rpc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() { 
+   srand(time(0));
+   int input;
+
+   char pos[3][8] = {"Rock", "Paper", "Scissors"};
+   
+   int comp_choice = rand() % 3; 
+   
+   printf("Welcome to my first real program in C: Rock, Paper, Scissors!\n\n");
+   
+   for (int i =0; i <3; i++)
+       printf("%s - [%d] ", pos[i], i);
+
+//   printf("%d", comp_choice);         THIS WAS FOR TESTING THE RANDOM NUMBER
+   while (1) {
+       printf("\n\nEnter a choice: [0-3]");
+       scanf(" %d", &input);
+       if (input >= 0 && input <= 2) {
+            break;
+       } 
+       printf("Invalid input. Please enter 0, 1, or 2.\n");
+   }
+
+
+   printf("\nComputer's choice is:%s", pos[comp_choice]);
+
+   printf("\n\nYour choice is:%s\n\n\n", pos[input]);
+
+
+   if ((input == 0 && comp_choice == 0) || (input == 1 && comp_choice == 1) || (input == 2 && comp_choice ==2)) {
+       printf("Tie");
+   } else if ((input == 0 && comp_choice == 1) || (input == 1 && comp_choice == 2) || (input == 2 && comp_choice == 0)) {
+       printf("You Lost :(");
+   } else if ((input == 0 && comp_choice == 2) || (input == 1 && comp_choice == 0) || (input == 2 && comp_choice == 1)) {
+       printf("You won :)");
+   }
+
+
+
+   return 0;
+}
+

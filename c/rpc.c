17981eae57fedc66e5dfd90511a2f481a4378e9f
Branson Bailey <bransonbailey1@gmail.com>
2026-03-29 17:53:18 -0400

    added error checkign and some other functionality to the RPC game.

--- c/rpc.c
+++ c/rpc.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
+
 int main() { 
+
+   bool play = true;
+   
+   printf("Welcome to my first real program in C: Rock, Paper, Scissors!\n\n");
+
+   while (play) {
+
    srand(time(0));
    int input;
 
@@ -9,18 +18,18 @@ int main() {
    
    int comp_choice = rand() % 3; 
    
-   printf("Welcome to my first real program in C: Rock, Paper, Scissors!\n\n");
-   
    for (int i =0; i <3; i++)
-       printf("%s - [%d] ", pos[i], i);
+       printf("\n\n%s - [%d] ", pos[i], i);
 
 //   printf("%d", comp_choice);         THIS WAS FOR TESTING THE RANDOM NUMBER
    while (1) {
-       printf("\n\nEnter a choice: [0-2]");
-       scanf(" %d", &input);
-       if (input >= 0 && input <= 2) {
+       printf("\n\nEnter a choice: ");
+       if(scanf(" %d", &input) == 1) {
+        if (input >= 0 && input <= 2) {
             break;
-       } 
+        }
+       }
+       while (getchar() != '\n');
        printf("Invalid input. Please enter 0, 1, or 2.\n");
    }
 
@@ -38,8 +47,29 @@ int main() {
        printf("You won :)");
    }
 
+   int play_choice;
+   
+   while (1){
+    printf("\n\nWould you like to play again?\n\nNo - [0] | Yes - [1]\n\nChoice: ");
+    if(scanf(" %d", &play_choice) == 1){
+        if (play_choice >= 0 && play_choice <= 1) {
+            break;    
+        }
+    }
+   
+    while (getchar() != '\n');
+    printf("Invalid input. Please enter 0 or 1\n");
+   }
+
+   play = (play_choice == 1);
+   if (play == 0) {
+    printf("\n\nGoodbye!\n\n\n");
+   }
 
 
-   return 0;
+    return 0;
+
+   }
+
 }
 

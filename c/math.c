a900b274ab3ace4ad026471b6e9a69653857551e
Branson Bailey <bransonbailey1@gmail.com>
2026-04-02 12:54:42 -0400

    practice with math

--- c/math.c
+++ c/math.c
@@ -15,12 +15,27 @@
  *
  * =====================================================================================
  */
-#include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    
-    return 0;
-}
+  int x = 9;
 
+  //    x = sqrt(x);    THIS IS FOR SQUARE ROOTS
+
+  // x = pow(x, 2);  THIS IS FOR POWERS (x,y) where x is the value and y is the
+  // power
+
+  // x = ceil(x);    THIS IS TO ROUND UP
+  // x = floor(x);   THIS IS TO ROUND DOWN
+  // x = abs(x); THIS GIVES YOU THE ABSOLUTE VALUE
+  // x = log(x); THIS GIVES YOU THE NATURAL LOGARITHM OF A VALUE
+  // x = sin(x); THIS GIVES YOU THE SIN OF A VALUE
+  // x = cos(x); THIS GIVES YOU THE COS OF A VALUE
+  // x = tan(x); THIS GIVES YOU THE TAN OF A VALUE
+
+  printf("%d", x);
+
+  return 0;
+}

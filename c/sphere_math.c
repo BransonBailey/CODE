66382a9d9e19ea747d2fa822592c41253bc22fc2
Branson Bailey <bransonbailey1@gmail.com>
2026-04-02 12:32:45 -0400

    workin on math stuff

---
+++ c/sphere_math.c
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sphere_math.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  04/02/2026 12:24:45 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *      Author(s):   (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  double radius = 0.0;
+  double area = 0.0;
+  const double PI = 3.14159;
+  double surfaceArea = 0.0;
+    double volume = 0.0;
+
+
+  printf("Enter the radius: ");
+  scanf("%lf", &radius);
+
+  area = PI * pow(radius, 2);
+  surfaceArea = 4 * PI * pow(radius, 2);
+    volume = (4.0 / 3.0) * PI * pow(radius, 3);
+  printf("Area: %.2lf\n", area);
+  printf("Surface Area: %.2lf\n", surfaceArea);
+  printf("Volume: %.2lf\n", volume);
+  return 0;
+}
